4ae33277b6955bb547c55c101eccdd72c2229dfe
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-29 10:20:06 +0200

    Initial commit of gh pages

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Flow Data Storage\u2019s documentation!"],titleterms:{data:0,document:0,flow:0,indic:0,storag:0,tabl:0,welcom:0}})
